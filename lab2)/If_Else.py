bd9ec39f36e824325cdcb16cad3c5e11b3894aa6
AdilYergazy <156777386+AdilYergazy@users.noreply.github.com>
2024-01-23 14:36:45 +0600

    If_Else.py

--- lab2)/If_Else.py
+++ lab2)/If_Else.py
@@ -1,11 +1,30 @@
 #Task 1 
 
+a = 50
+b = 10
+if a > b:
+  print("Hello World")
 #Task 2 
-
+a = 50
+b = 10
+if a != b:
+  print("Hello World")
 #Task 3 
-
+a = 50
+b = 10
+if a == b:
+  print("Yes")
+else:
+  print("No")
 #Task 4 
-
+a = 50
+b = 10
+if a == b:
+  print("1")
+elif a > b:
+  print("2")
+else:
+  print("3")
 #Task 5 
 
 #Task 6 
